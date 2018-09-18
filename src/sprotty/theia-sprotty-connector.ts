6daaab8407147981a3146cb2c3d1b66a4c064b33
Eugen Neufeld <eneufeld@eclipsesource.com>
2018-09-18 14:36:05 +0200

    Update to be compatible with theia 0.3.14

--- src/sprotty/theia-sprotty-connector.ts
+++ src/sprotty/theia-sprotty-connector.ts
@@ -9,9 +9,7 @@
 
 import { ActionMessage, ExportSvgAction, ServerStatusAction } from 'glsp-sprotty/lib'
 import { TheiaDiagramServer } from './theia-diagram-server'
-import { NotificationType } from 'vscode-jsonrpc/lib/messages'
-import { Location } from 'vscode-languageserver-types'
-import { LanguageClientContribution } from '@theia/languages/lib/browser'
+import { LanguageClientContribution, Location, NotificationType } from '@theia/languages/lib/browser'
 import { EditorManager } from '@theia/editor/lib/browser'
 import { TheiaFileSaver } from './theia-file-saver'
 import { DiagramWidgetRegistry } from '../theia/diagram-widget-registry'
